0651feee1708c84aa94399c5701ce82e4ec4f6a1
KhattabKHA5 <105236641+KhattabKHA5@users.noreply.github.com>
2023-11-20 14:00:04 +0200

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
+
 document.addEventListener('DOMContentLoaded', function () {
     const postButton = document.getElementById('postButton');
     const postsContainer = document.getElementById('postsContainer');
 
     postButton.addEventListener('click', function () {
-        const postContent = prompt('أدخل نص, صورة, أو رابط فيديو:');
-        if (postContent) {
-            const postElement = document.createElement('div');
-            postElement.classList.add('post');
+        const postType = prompt('اختر نوع المنشور: "نص" أو "فيديو/صورة"');
+        
+        if (postType) {
+            if (postType.toLowerCase() === 'نص') {
+                const textContent = prompt('أدخل النص:');
+                if (textContent) {
+                    createPost('نص: ' + textContent);
+                }
+            } else if (postType.toLowerCase() === 'فيديو/صورة') {
+                const fileInput = document.createElement('input');
+                fileInput.type = 'file';
+                fileInput.accept = 'image/*, video/*';
 
-            // اضف الكود هنا لعرض المحتوى بناءً على نوعه (نص، صورة، أو فيديو)
+                fileInput.addEventListener('change', function () {
+                    const file = fileInput.files[0];
+                    if (file) {
+                        const textContent = prompt('أدخل نصاً للصورة أو الفيديو:');
+                        if (textContent) {
+                            createPost('صورة/فيديو: ' + textContent, file);
+                        }
+                    }
+                });
 
-            postsContainer.appendChild(postElement);
+                fileInput.click();
+            } else {
+                alert('اختيار غير صالح');
+            }
         }
     });
+
+    function createPost(content, file) {
+        const postElement = document.createElement('div');
+        postElement.classList.add('post');
+        postElement.innerHTML = content;
+
+        if (file) {
+            const mediaElement = document.createElement(file.type.startsWith('image') ? 'img' : 'video');
+            mediaElement.src = URL.createObjectURL(file);
+            mediaElement.controls = true;
+            mediaElement.style.width = '100%';
+            postElement.appendChild(mediaElement);
+        }
+
+        postsContainer.appendChild(postElement);
+    }
 });
