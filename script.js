e95bd60902c0c8daa4e1c8ccf69d08b4c9afd083
KhattabKHA5 <105236641+KhattabKHA5@users.noreply.github.com>
2023-11-20 14:49:39 +0200

    script.js

--- script.js
+++ script.js
@@ -1,117 +1,57 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const postsContainer = document.getElementById('postsContainer');
-    const actionsContainer = document.querySelector('.actions-container');
-    const shareButton = document.getElementById('shareButton');
-    const shareOptions = document.getElementById('shareOptions');
-    const savedUserName = localStorage.getItem('userName');
-    let userName;
+  const publishBtn = document.getElementById('publish-btn');
+  const publishOptions = document.getElementById('publish-options');
+  const textInput = document.getElementById('text-input');
+  const mediaInput = document.getElementById('media-input');
+  const textPublishBtn = document.getElementById('text-publish-btn');
+  const mediaPublishBtn = document.getElementById('media-publish-btn');
+  const postsContainer = document.getElementById('posts-container');
 
-    if (savedUserName) {
-        userName = savedUserName;
-    } else {
-        userName = prompt('مرحبًا! الرجاء إدخال اسمك:');
-        localStorage.setItem('userName', userName);
-    }
+  publishBtn.addEventListener('click', function () {
+    publishOptions.style.display = 'block';
+  });
 
-    fetchAndDisplayPosts();
+  textPublishBtn.addEventListener('click', function () {
+    const textContent = document.getElementById('text-content').value;
+    createTextPost(textContent);
+  });
 
-    function fetchAndDisplayPosts() {
-        // استرجاع المنشورات من Local Storage
-        const savedPosts = JSON.parse(localStorage.getItem('posts')) || [];
-        savedPosts.forEach(post => createPost(post.content, post.userName, post.file));
-    }
+  mediaPublishBtn.addEventListener('click', function () {
+    const mediaFile = document.getElementById('media-file').files[0];
+    const mediaCaption = document.getElementById('media-caption').value;
+    createMediaPost(mediaFile, mediaCaption);
+  });
 
-    function toggleShareOptions() {
-        shareOptions.style.display = shareOptions.style.display === 'flex' ? 'none' : 'flex';
-    }
+  function createTextPost(content) {
+    const post = document.createElement('div');
+    post.className = 'post';
+    post.innerHTML = `<p>${content}</p><span class="delete-btn" onclick="deletePost(this)">حذف</span>`;
+    postsContainer.appendChild(post);
+    resetInputs();
+  }
 
-    function shareText() {
-        const textContent = prompt('أدخل النص:');
-        if (textContent) {
-            createPost('اسم الناشر: ' + userName + '<br>' + textContent, userName);
-            toggleShareOptions();
-        }
-    }
+  function createMediaPost(file, caption) {
+    const post = document.createElement('div');
+    post.className = 'post';
+    const mediaElement = file.type.includes('image')
+      ? `<img src="${URL.createObjectURL(file)}" alt="صورة" class="media-item">`
+      : `<video src="${URL.createObjectURL(file)}" controls class="media-item"></video>`;
+    post.innerHTML = `${mediaElement}<p>${caption}</p><span class="delete-btn" onclick="deletePost(this)">حذف</span>`;
+    postsContainer.appendChild(post);
+    resetInputs();
+  }
 
-    function shareMedia() {
-        const fileInput = document.createElement('input');
-        fileInput.type = 'file';
-        fileInput.accept = 'image/*, video/*';
+  function resetInputs() {
+    document.getElementById('text-content').value = '';
+    document.getElementById('media-file').value = '';
+    document.getElementById('media-caption').value = '';
+    publishOptions.style.display = 'none';
+    textInput.style.display = 'none';
+    mediaInput.style.display = 'none';
+  }
 
-        fileInput.addEventListener('change', function () {
-            const file = fileInput.files[0];
-            if (file) {
-                const textContent = prompt('أدخل نصاً للصورة أو الفيديو:');
-                if (textContent) {
-                    createPost('اسم الناشر: ' + userName + '<br>' + textContent, userName, file);
-                    toggleShareOptions();
-                }
-            }
-        });
-
-        fileInput.click();
-    }
-
-    function createPost(content, userName, file) {
-        const postElement = document.createElement('div');
-        postElement.classList.add('post');
-
-        if (file) {
-            const mediaElement = document.createElement(file.type.startsWith('image') ? 'img' : 'video');
-            mediaElement.src = URL.createObjectURL(file);
-            mediaElement.controls = true;
-            mediaElement.style.width = '100%';
-            postElement.appendChild(mediaElement);
-        }
-
-        const postContentElement = document.createElement('div');
-        postContentElement.classList.add('post-content');
-        postContentElement.innerHTML = content;
-
-        // أضف زر حذف
-        const deleteButton = document.createElement('button');
-        deleteButton.classList.add('delete-button');
-        deleteButton.textContent = 'حذف';
-
-        deleteButton.addEventListener('click', function () {
-            postsContainer.removeChild(postElement);
-            savePosts();
-        });
-
-        // أضف زر تعليق
-        const commentButton = document.createElement('button');
-        commentButton.classList.add('comment-button');
-        commentButton.textContent = 'تعليق';
-
-        commentButton.addEventListener('click', function () {
-            const commentContent = prompt('أدخل تعليق:');
-            if (commentContent) {
-                const commentElement = document.createElement('div');
-                commentElement.innerHTML = '<strong>' + userName + ':</strong> ' + commentContent;
-                postContentElement.appendChild(commentElement);
-                savePosts();
-            }
-        });
-
-        postElement.appendChild(postContentElement);
-        postElement.appendChild(deleteButton);
-        postElement.appendChild(commentButton);
-
-        postsContainer.appendChild(postElement);
-
-        // حفظ المنشورات في Local Storage
-        savePosts();
-    }
-
-    function savePosts() {
-        const allPosts = Array.from(document.querySelectorAll('.post')).map(postElement => {
-            const content = postElement.querySelector('.post-content').innerHTML;
-            const fileElement = postElement.querySelector('img, video');
-            const file = fileElement ? fileElement.src : null;
-
-            return { content, userName, file };
-        });
-
-        localStorage.setItem('posts', JSON.stringify(allPosts));
-    }
+  window.deletePost = function (deleteBtn) {
+    const post = deleteBtn.parentNode;
+    postsContainer.removeChild(post);
+  };
 });
