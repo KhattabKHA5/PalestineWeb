66e46b7fbfd8b854ebb6457f2b89280021ba9389
KhattabKHA5 <105236641+KhattabKHA5@users.noreply.github.com>
2023-11-20 14:44:17 +0200

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     const postsContainer = document.getElementById('postsContainer');
+    const actionsContainer = document.querySelector('.actions-container');
+    const shareButton = document.getElementById('shareButton');
+    const shareOptions = document.getElementById('shareOptions');
     const savedUserName = localStorage.getItem('userName');
     let userName;
 
@@ -18,37 +21,36 @@ document.addEventListener('DOMContentLoaded', function () {
         savedPosts.forEach(post => createPost(post.content, post.userName, post.file));
     }
 
-    const postButton = document.getElementById('postButton');
-    postButton.addEventListener('click', function () {
-        const postType = prompt('اختر نوع المنشور: "نص" أو "فيديو/صورة"');
+    function toggleShareOptions() {
+        shareOptions.style.display = shareOptions.style.display === 'flex' ? 'none' : 'flex';
+    }
 
-        if (postType) {
-            if (postType.toLowerCase() === 'نص') {
-                const textContent = prompt('أدخل النص:');
-                if (textContent) {
-                    createPost('اسم الناشر: ' + userName + '<br>' + textContent, userName);
-                }
-            } else if (postType.toLowerCase() === 'فيديو/صورة') {
-                const fileInput = document.createElement('input');
-                fileInput.type = 'file';
-                fileInput.accept = 'image/*, video/*';
-
-                fileInput.addEventListener('change', function () {
-                    const file = fileInput.files[0];
-                    if (file) {
-                        const textContent = prompt('أدخل نصاً للصورة أو الفيديو:');
-                        if (textContent) {
-                            createPost('اسم الناشر: ' + userName + '<br>' + textContent, userName, file);
-                        }
-                    }
-                });
-
-                fileInput.click();
-            } else {
-                alert('اختيار غير صالح');
-            }
+    function shareText() {
+        const textContent = prompt('أدخل النص:');
+        if (textContent) {
+            createPost('اسم الناشر: ' + userName + '<br>' + textContent, userName);
+            toggleShareOptions();
         }
-    });
+    }
+
+    function shareMedia() {
+        const fileInput = document.createElement('input');
+        fileInput.type = 'file';
+        fileInput.accept = 'image/*, video/*';
+
+        fileInput.addEventListener('change', function () {
+            const file = fileInput.files[0];
+            if (file) {
+                const textContent = prompt('أدخل نصاً للصورة أو الفيديو:');
+                if (textContent) {
+                    createPost('اسم الناشر: ' + userName + '<br>' + textContent, userName, file);
+                    toggleShareOptions();
+                }
+            }
+        });
+
+        fileInput.click();
+    }
 
     function createPost(content, userName, file) {
         const postElement = document.createElement('div');
@@ -76,8 +78,24 @@ document.addEventListener('DOMContentLoaded', function () {
             savePosts();
         });
 
+        // أضف زر تعليق
+        const commentButton = document.createElement('button');
+        commentButton.classList.add('comment-button');
+        commentButton.textContent = 'تعليق';
+
+        commentButton.addEventListener('click', function () {
+            const commentContent = prompt('أدخل تعليق:');
+            if (commentContent) {
+                const commentElement = document.createElement('div');
+                commentElement.innerHTML = '<strong>' + userName + ':</strong> ' + commentContent;
+                postContentElement.appendChild(commentElement);
+                savePosts();
+            }
+        });
+
         postElement.appendChild(postContentElement);
         postElement.appendChild(deleteButton);
+        postElement.appendChild(commentButton);
 
         postsContainer.appendChild(postElement);
 
